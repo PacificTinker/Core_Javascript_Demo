a921f4929f9130a6782a18dc47ad927ef14a4b0c
PacificTinker <github@bayvaricomp.com>
2018-10-01 23:52:12 +0800

    Create switch.js
    Uses switch for control of flow

---
+++ switch.js
@@ -0,0 +1,16 @@
+var moonPhase = 'fool';
+
+switch (moonPhase) {    
+case 'full':
+  console.log('Howwwwllll!!')
+  break;
+case 'mostly full':
+  console.log('Arms and legs are getting hairier')
+  break;
+case 'mostly new':
+  console.log('Back on two feet')
+  break;
+default:
+  console.log('Invalid moon phase');
+  break;
+}
