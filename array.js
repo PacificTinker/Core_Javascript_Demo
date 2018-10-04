22a82ef64e9b072c1e35da5aec7066d26d7c68b2
PacificTinker <github@bayvaricomp.com>
2018-10-04 21:53:02 +0800

    Create array.js
    Basic demonstration of an array in Javascript

---
+++ array.js
@@ -0,0 +1,5 @@
+var bucketList = ['learn to juggle', 'take a falconry class', 'climb Mt Everest'];
+
+bucketList.push('score the winning touchdown at the super bowl', 'start an alpaca farm');
+
+console.log(bucketList);
